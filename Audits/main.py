b5835af1a039b5cd35df033f18db7946a868282f
SeiyuIsobe <iseiyu@hotmail.co.jp>
2025-10-13 09:51:32 +0900

    ok

--- Audits/main.py
+++ Audits/main.py
@@ -1,6 +1,15 @@
+"""
+ThingWorx/Composer/ExportOnlineAuditData
+上記APIから出力されるファイルを読込み、SMS用のフォーマットに整形する
+
+改訂履歴：
+2025/10/07  jsonファイルを探索するロジックの変更
+            SMSの要求するCategoryに変更する機能の追加
+"""
 import json
 import glob
 from datetime import datetime
+import pathlib
 
 """
 {
@@ -20,21 +29,68 @@ from datetime import datetime
 
 
 # 定義
-#TARGET_JSON = r"C:\Users\s-iso\Downloads\TWL00837_241001_250331_all\AuditArchive\AuditArchiveDirectPersistence\export\*.json"
-#TARGET_JSON = r"AuditArchive\AuditArchiveDirectPersistence\export\*.json"
-TARGET_JSON = r"C:\temp\temp5\act2\AuditArchive\AuditArchiveDirectPersistence\export\*.json"
-OUTPUT_CSV = "audit_log.csv"
-IS_SOURCE_TWL = True # sourceが入っているもののみ抽出=True
+#TARGET_JSON = r"C:\Users\s-iso\Downloads\sms_202504_202509\AuditArchive\AuditArchiveDirectPersistence\export\*.json"
+TARGET_JSON = r"C:\temp\temp6\*.json"
+TARGET_ROOT = r"C:\temp\temp6\out"
+OUTPUT_CSV = "audit_log_all_test.csv"
+IS_SOURCE_TWL = True # sourceがThingNameであるもののみ抽出=True、全部=False
+
+_dic_cat = {
+    "IDM WS Extension": "Information",
+    "Maintenance Window": "Information",
+    "User Alerts": "Information",
+    "Alarm Acknowledge": "Loｗ",
+    "Asset Communication": "Medium",
+    "Code Access Policy": "Information",
+    "Custom": "Information",
+    "Custom Object": "Information",
+    "Custom Object Group": "Information",
+    "Data Export": "Critical",
+    "Data Management": "Medium",
+    "Data Streaming": "Information",
+    "Extended Data": "Information",
+    "External Credential": "High",
+    "File Transfer": "Low",
+    "File Upload": "Low",
+    "Maintenance": "Medium",
+    "Network": "Medium",
+    "Package Management": "Information",
+    "Partner Login": "High",
+    "Remote Access": "Critical",
+    "Rules Configuration": "Medium",
+    "Scripting": "Critical",
+    "System Configuration": "Medium",
+    "Transport ServiceLink": "Information",
+    "User Access": "High",
+    "Modeling": "Medium"
+}
+
+def getLogFileList(pt, ext):
+    path = pathlib.Path(pt)
+    files = path.glob(ext)
+    return files
+
+def convertForSMSCategory(fromCat):
+    try:
+        val = _dic_cat[fromCat]
+        return val
+    except:
+        pass
+    return None
 
 def main():
     
     #JSONファイルのリスト取得
-    jsons = glob.glob(TARGET_JSON)
+    #jsons = glob.glob(TARGET_JSON)
+    jsons = getLogFileList(TARGET_ROOT, "**/*.json")
+
+
     
     try:
         #ヘッダーの書込み
         fheader = open(OUTPUT_CSV, "w") #既存をクリアのモード
-        fheader.writelines("auditCategory,application,sourceType,source,message,user,timestamp(UTC)\n")
+        fheader.writelines("timestamp(UTC),Cat,Category,Description,user,Asset,不要,不要\n")
+        fheader.writelines("timestamp(UTC),,auditCategory,message,user,source,application,sourceType\n") #変換後が分かるように残すため
         fheader.close()
     
     except Exception as e3:
@@ -70,14 +126,15 @@ def main():
                         isOut = True
 
                     if isOut:
-                        fw.writelines("{0},{1},{2},{3},{4},{5},{6}\n".format(
+                        fw.writelines("{0},{1},{2},{3},{4},{5},{6},{7}\n".format(
+                            datetime.fromtimestamp(row["timestamp"]/1000), #unixtimeは1000で割る
+                            convertForSMSCategory(row["auditCategory"]),
                             row["auditCategory"],
-                            row["application"],
-                            row["sourceType"],
-                            row["source"],
                             row["message"],
                             row["user"],
-                            datetime.fromtimestamp(row["timestamp"]/1000) #unixtimeは1000で割る
+                            row["source"],
+                            row["application"],
+                            row["sourceType"]
                             )
                         )
                     
